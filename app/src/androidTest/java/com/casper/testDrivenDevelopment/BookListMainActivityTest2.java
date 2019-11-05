a68466a47bd3f90dd3ec80e51b5e522e5af5c883
casper chen <70838697@qq.com>
2019-11-05 23:53:02 +0800

    test list view contextmenu

---
+++ app/src/androidTest/java/com/casper/testDrivenDevelopment/BookListMainActivityTest2.java
@@ -0,0 +1,255 @@
+package com.casper.testDrivenDevelopment;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.ImageView;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class BookListMainActivityTest2 {
+
+    @Rule
+    public ActivityTestRule<BookListMainActivity> mActivityTestRule = new ActivityTestRule<>(BookListMainActivity.class);
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    private static Matcher<View> hasDrawableResource(final int resourceId) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Imageview has a resource id of " + resourceId);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ImageView imageView = (ImageView) view;
+                return imageView.getDrawable() != null &&
+                        imageView.getDrawable().getCurrent().getConstantState().equals(imageView.getResources().getDrawable(resourceId).getConstantState());
+            }
+        };
+    }
+
+    @Test
+    public void testContextMenuNewAndDelete() {
+        List<Book> listBooks = mActivityTestRule.getActivity().getListBooks();
+        int bookCount=listBooks.size();
+
+        String testText = "无名书籍";
+        int resourceId = R.drawable.book_no_name;
+
+        /*检查要测试的对象不存在*/
+        ViewInteraction theNotExistTextView = onView(
+                allOf(withId(R.id.text_view_book_title), withText(testText)));
+        theNotExistTextView.check(
+                doesNotExist());
+
+        Assert.assertNotEquals(testText,listBooks.get(0).getTitle());
+        Assert.assertNotEquals(resourceId,listBooks.get(0).getCoverResourceId());
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.image_view_book_cover),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.list_view_books),
+                                        0),
+                                0),
+                        isDisplayed()));
+        imageView.check(matches(not(hasDrawableResource(resourceId))));
+
+        /*新建菜单不存在*/
+        ViewInteraction textViewMenu = onView(
+                allOf(withId(android.R.id.title), withText("添加"), isDisplayed()));
+        textViewMenu.check(doesNotExist());
+
+        /*第一个对象长按*/
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        withId(R.id.list_view_books),
+                        0),
+                        isDisplayed()));
+        linearLayout.perform(longClick());
+
+        /*出现新建菜单*/
+        onView(
+                allOf(withId(android.R.id.title), withText(listBooks.get(0).getTitle()))).check(matches(isDisplayed()));
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.title), withText("添加"), isDisplayed()));
+        textView.perform(click());
+        ViewInteraction theExistTextView = onView(
+                allOf(withId(R.id.text_view_book_title),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.list_view_books),
+                                        0),
+                                1),
+                        withText(testText)));
+        theExistTextView.check(matches(isDisplayed()));
+
+        Assert.assertEquals(testText,listBooks.get(0).getTitle());
+        Assert.assertEquals(resourceId,listBooks.get(0).getCoverResourceId());
+        Assert.assertEquals(bookCount+1,listBooks.size());
+
+        imageView = onView(
+                allOf(withId(R.id.image_view_book_cover),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.list_view_books),
+                                        0),
+                                0),
+                        isDisplayed()));
+        imageView.check(matches((hasDrawableResource(listBooks.get(0).getCoverResourceId()))));
+
+
+        /*删除菜单不存在*/
+        ViewInteraction textViewMenu1 = onView(
+                allOf(withId(android.R.id.title), withText("删除"), isDisplayed()));
+        textViewMenu1.check(doesNotExist());
+        /*第一个对象长按*/
+        ViewInteraction linearLayout1 = onView(
+                allOf(childAtPosition(
+                        withId(R.id.list_view_books),
+                        0),
+                        isDisplayed()));
+        linearLayout1.perform(longClick());
+
+        /*出现删除菜单*/
+        onView(
+                allOf(withId(android.R.id.title), withText("删除"), isDisplayed()))
+                .perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (
+                InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("您确定要删除这条吗？"))
+                .inRoot(withDecorView(not(mActivityTestRule.getActivity()
+                        .getWindow()
+                        .getDecorView())))
+                .check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(
+                allOf(IsInstanceOf.<View>instanceOf(android.widget.TextView.class), withText("询问"),
+                        isDisplayed()))
+                .inRoot(withDecorView(not(mActivityTestRule.getActivity()
+                .getWindow()
+                .getDecorView())))
+                ;
+        textView2.check(matches(withText("询问")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(android.R.id.message), withText("您确定要删除这条吗？"),
+                        isDisplayed()));
+        textView4.check(matches(withText("您确定要删除这条吗？")));
+
+        ViewInteraction appCompatButton = onView(allOf(withId(android.R.id.button2), withText("取消")))
+                        .perform(scrollTo(), click());
+        Assert.assertEquals(testText,listBooks.get(0).getTitle());
+        Assert.assertEquals(resourceId,listBooks.get(0).getCoverResourceId());
+        Assert.assertEquals(bookCount+1,listBooks.size());
+
+        /*-----------------------测试删除-----------------------------*/
+        /*第一个对象长按*/
+        onView(
+                allOf(childAtPosition(
+                        withId(R.id.list_view_books),
+                        0),
+                        isDisplayed()))
+                .perform(longClick());
+
+        /*出现删除菜单*/
+        onView(allOf(withId(android.R.id.title), withText("删除"), isDisplayed()))
+                .perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (
+                InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("您确定要删除这条吗？"))
+                .inRoot(withDecorView(not(mActivityTestRule.getActivity()
+                        .getWindow()
+                        .getDecorView())))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(android.R.id.button1), withText("确定")))
+                .perform(scrollTo(), click());
+        Assert.assertNotEquals(testText,listBooks.get(0).getTitle());
+        Assert.assertNotEquals(resourceId,listBooks.get(0).getCoverResourceId());
+        Assert.assertEquals(bookCount,listBooks.size());
+        onView(allOf(withId(R.id.text_view_book_title), withText(testText)))
+                .check(
+                doesNotExist());
+
+        Assert.assertNotEquals(testText,listBooks.get(0).getTitle());
+        Assert.assertNotEquals(resourceId,listBooks.get(0).getCoverResourceId());
+
+        onView(allOf(withId(R.id.image_view_book_cover),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.list_view_books),
+                                        0),
+                                0),
+                        isDisplayed()))
+                .check(matches(not(hasDrawableResource(resourceId))));
+    }
+
+}
--- app/src/main/java/com/casper/testDrivenDevelopment/BookListMainActivity.java
+++ app/src/main/java/com/casper/testDrivenDevelopment/BookListMainActivity.java
@@ -1,40 +1,100 @@
 package com.casper.testDrivenDevelopment;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookListMainActivity extends AppCompatActivity {
 
-    private ListView listViewBooks;
+    public static final int CONTEXT_MENU_DELETE = 1;
+    public static final int CONTEXT_MENU_NEW = CONTEXT_MENU_DELETE + 1;
+    public static final int CONTEXT_MENU_ABOUT = CONTEXT_MENU_NEW + 1;
     private List<Book> listBooks = new ArrayList<>();
+    ListView listViewBooks;
+    BookAdapter bookAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list_main);
 
         init();
+        listViewBooks = this.findViewById(R.id.list_view_books);
 
-        listViewBooks=findViewById(R.id.list_view_books);
+        bookAdapter = new BookAdapter(BookListMainActivity.this, R.layout.list_view_item_book, getListBooks());
+        listViewBooks.setAdapter(bookAdapter);
 
-        BookAdapter adapter = new BookAdapter(BookListMainActivity.this, R.layout.list_view_item_book, getListBooks());
-        listViewBooks.setAdapter(adapter);
+        this.registerForContextMenu(listViewBooks);
     }
-    /**
-     * 初始化数据
-     */
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if (v == listViewBooks) {
+            //获取适配器
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            //设置标题
+            menu.setHeaderTitle(listBooks.get(info.position).getTitle());
+            //设置内容 参数1为分组，参数2对应条目的id，参数3是指排列顺序，默认排列即可
+            menu.add(0, CONTEXT_MENU_DELETE, 0, "删除");
+            menu.add(0, CONTEXT_MENU_NEW, 0, "添加");
+            menu.add(0, CONTEXT_MENU_ABOUT, 0, "关于...");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case CONTEXT_MENU_DELETE:
+                final int removePosition = ((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).position;
+                Dialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("询问")
+                        .setMessage("您确定要删除这条吗？")
+                        .setIcon(R.drawable.ic_launcher_foreground)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                listBooks.remove(removePosition);
+                                bookAdapter.notifyDataSetChanged();
+                                Toast.makeText(BookListMainActivity.this, "删除成功", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).create();
+                dialog.show();
+                break;
+            case CONTEXT_MENU_NEW:
+                final int insertPosition = ((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).position;
+                getListBooks().add(insertPosition, new Book("无名书籍", R.drawable.book_no_name));
+                bookAdapter.notifyDataSetChanged();
+                break;
+            case CONTEXT_MENU_ABOUT:
+                Toast.makeText(BookListMainActivity.this, "图书列表v1.0,coded by casper", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
     private void init() {
         getListBooks().add(new Book("软件项目管理案例教程（第4版）", R.drawable.book_2));
         getListBooks().add(new Book("创新工程实践", R.drawable.book_no_name));
@@ -49,7 +109,7 @@ public class BookListMainActivity extends AppCompatActivity {
 
         private int resourceId;
 
-        public BookAdapter(Context context, int resource, List<Book> objects) {
+        BookAdapter(Context context, int resource, List<Book> objects) {
             super(context, resource, objects);
             resourceId = resource;
         }
@@ -65,4 +125,3 @@ public class BookListMainActivity extends AppCompatActivity {
         }
     }
 }
-
